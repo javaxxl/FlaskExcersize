e25f5ac772f99aa1c20f55d87876d8f0586d1e3b
javaxxl <javaxxl@163.com>
2018-12-19 21:05:33 +0800

    181219.01 commit

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ Created on 2018/12/14 - the current system date.
 __auther__ = 'xiaoliang'
 """
 
-from flask import Flask, request, make_response, redirect, abort, render_template
+from flask import Flask, request, make_response, redirect, abort, render_template, url_for, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What\'s your name?', validators=[DataRequired()])
+    submit = SubmitField('submit')
+
+
 
 
 app = Flask("try1")
@@ -30,6 +39,24 @@ def user(name):
     # return '<h1>nice to meet you, %s!</h1>' % name
     return render_template('user3.html', name=name)
 
+@app.route('/ask/', methods=['GET','POST'])
+def ask():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('ask.html', form=form, name=name)
+
+@app.route('/info/', methods=['GET','POST'])
+def info():
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('info'))
+    return  render_template('info.html', form=form, name=session.get('name'))
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -49,6 +76,11 @@ def get_user(id):
 """
 
 
+
+
+
+
 if __name__ == "__main__":
+    app.config['SECRET_KEY'] = 'HARD TO GUESS STRING'
     bootstrap = Bootstrap(app)
     app.run(debug=True)
