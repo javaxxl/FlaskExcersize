88dc5ac12342ffcaa5197b26a76112a63da505be
javaxxl <javaxxl@163.com>
2018-12-23 14:38:44 +0800

    181223.01 commits

--- main.py
+++ main.py
@@ -5,41 +5,81 @@ Created on 2018/12/14 - the current system date.
 __auther__ = 'xiaoliang'
 """
 
-from flask import Flask, request, make_response, redirect, abort, render_template, url_for, session
+from flask import Flask,  redirect, render_template, url_for, session
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'HARD TO GUESS STRING'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:passw0rd@127.0.0.1:3306/Flasky'
+# 来启用自动提交数据库更改在每个请求中
+# 由SQLAlchemy实例化的db对象表示数据库且提供访问Flask-SQLAlchemy的所有功能
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
+
+
 
 class NameForm(FlaskForm):
     name = StringField('What\'s your name?', validators=[DataRequired()])
     submit = SubmitField('submit')
 
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    # users = db.relationship('User', backref='role', lazy='subquery')
+    users = db.relationship('User', backref='role',lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
 
 
-app = Flask("try1")
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    # roles = db.relationship('Role', backref='user')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    user_agent = request.headers.get('User-Agent')
-    # return "<h1>Bad request</h1>", 400
-    # return '<h1>your browser agent: %s</h1>' % user_agent
-    """
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer', '42')
-    return response
-    """
-    return redirect('https://www.baidu.com')
+    # return render_template('user3.html',name='Rock')
 
+
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form, name=session.get('name'), known=session.get('known', False))
 
 @app.route('/user/<name>')
 def user(name):
     # return '<h1>nice to meet you, %s!</h1>' % name
     return render_template('user3.html', name=name)
 
-@app.route('/ask/', methods=['GET','POST'])
+
+
+@app.route('/ask/', methods=['GET', 'POST'])
 def ask():
     name = None
     form = NameForm()
@@ -48,39 +88,26 @@ def ask():
         form.name.data = ''
     return render_template('ask.html', form=form, name=name)
 
-@app.route('/info/', methods=['GET','POST'])
+
+@app.route('/info/', methods=['GET', 'POST'])
 def info():
     form = NameForm()
     if form.validate_on_submit():
         session['name'] = form.name.data
         return redirect(url_for('info'))
-    return  render_template('info.html', form=form, name=session.get('name'))
+    return render_template('info.html', form=form, name=session.get('name'))
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def interal_server_error(e):
     return render_template('500.html'), 500
 
 
-"""
-@app.route('/user/<id>')
-def get_user(id):
-    user = load_user(id)
-    if not user:
-        abort(404)
-        return '<h1>Hello, %s</h1>' % user.name
-"""
 
-
-
-
-
-
-if __name__ == "__main__":
-    app.config['SECRET_KEY'] = 'HARD TO GUESS STRING'
-    bootstrap = Bootstrap(app)
+if __name__ == '__main__':
     app.run(debug=True)
